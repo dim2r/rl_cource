e2129a7b45767442bca3f3304e08c8eac07f5469
D <dim3r@yandex.ru>
2021-12-07 00:29:34 +0400

    ph

--- src/install_and_run_for_windows.bat
+++ src/install_and_run_for_windows.bat
@@ -3,6 +3,7 @@ set env_name=myenv
 
 @echo ----------------------
 @%PYTHON% --version
+@%PYTHON% -c "import sys;print(sys.executable)"
 @echo ----------------------
 
 %PYTHON% -m pip install --upgrade pip
@@ -19,29 +20,17 @@ set PYTHON=.\%env_name%\Scripts\python.exe
 @echo ---------------------- Installing packages into %env_name%   ----------------------
 %PIP% install jupyterlab
 
-cd /d %env_name%\Scripts 
-.\activate 
-cd /d    ..\..\ 
+cd /d %env_name%\Scripts\activate 
 
 pwd
 @%PYTHON% --version
-pause
 
 rem %PIP% install wheel
 rem %PIP% install six
 rem %PIP% install torch torchvision gym gym[atari] numpngw numpy jdc  unrar matplotlib  
-%PIP% install torch torchvision gym numpngw numpy pyglet PyYAML jdc    
+%PIP% install torch torchvision gym numpngw numpy pyglet PyYAML jdc unrar  matplotlib  
 %PIP% install pybullet
 %PIP% install git+https://github.com/benelot/pybullet-gym
-
-rem --------------- BEGIN: CHOOSE ONE OPTION
-rem %PIP% install atari_py
-rem %PIP% install -f https://github.com/Kojoley/atari-py/releases atari_py
-rem --------------- END
-
-
-
-%PYTHON% -m atari_py.import_roms rom
 
 
 @echo ---------------------- starting ----------------------
@@ -49,5 +38,6 @@ rem %PYTHON% PG_torch.py
 
 rem .\%env_name%\Scripts\activate
 
+.\%env_name%\Scripts\jupyter-lab.exe   TRPO+PPO_homework.ipynb
 
 pause
